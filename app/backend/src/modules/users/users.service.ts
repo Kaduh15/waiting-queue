779eef8342a6367c64d4b69ce9dc4c715982be96
kaduh15 <kadu.siva2014@gmail.com>
2023-01-23 08:57:58 -0300

    feat(backend): implementa o crud na rota de usuários

--- app/backend/src/modules/users/users.service.ts
+++ app/backend/src/modules/users/users.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/Prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
   async create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.prisma.user.create({
+      data: createUserDto,
+    });
   }
 
   async findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
   async findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.prisma.user.delete({
+      where: { id },
+    });
   }
 }
