9ad5267ff201c46774a010adb1505cd67c8091b8
kaduh15 <kadu.siva2014@gmail.com>
2023-01-25 15:51:05 -0300

    test(bc-waiting-line-router): cria teste para adicionar um cliente 🧪

--- app/backend/src/modules/waiting-line/entities/waiting-line.entity.ts
+++ app/backend/src/modules/waiting-line/entities/waiting-line.entity.ts
@@ -1,9 +1,22 @@
+import { IsDate, IsString, IsUUID } from "class-validator";
+
 export class WaitingLine {
+  @IsUUID()
   id?: string;
+
+  @IsString()
   name: string;
+
+  @IsDate()
   createdAt: Date;
+  
+  @IsString()
   status: Status;
+
+  @IsDate()
   initialServiceTime?: Date
+  
+  @IsDate()
   finishedServiceTime?: Date
 }
 
--- app/backend/src/modules/waiting-line/waiting-line.service.spec.ts
+++ app/backend/src/modules/waiting-line/waiting-line.service.spec.ts
@@ -1,18 +1,48 @@
+import { CreateWaitingLineDto } from './dto/create-waiting-line.dto';
+import { PrismaService } from './../../prisma/Prisma.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { WaitingLineService } from './waiting-line.service';
+import { WaitingLine } from './entities/waiting-line.entity';
 
 describe('WaitingLineService', () => {
   let service: WaitingLineService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WaitingLineService],
+      providers: [PrismaService,WaitingLineService],
     }).compile();
 
     service = module.get<WaitingLineService>(WaitingLineService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('create', () => {
+    it('should create a waiting line', async () => {
+      const name = 'Test'
+
+      const clientInput: CreateWaitingLineDto = {
+        name
+      };
+
+      const clientOutput: WaitingLine = {
+        id: 'uuid',
+        name,
+        createdAt: new Date(),
+        status: 'WAITING',
+        initialServiceTime: null,
+        finishedServiceTime: null
+      }
+
+      jest.spyOn(prisma.waitingLine, 'create').mockImplementation(() => clientOutput as any)
+
+      const waitingLine = await service.create(clientInput);
+
+      expect(waitingLine).toEqual(clientOutput);
+    });
+  })
 });
