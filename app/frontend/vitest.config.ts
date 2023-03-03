254db2b5031f8799ce4161b053fe80d483856ebd
kaduh15 <kadu.siva2014@gmail.com>
2023-03-03 08:18:05 -0300

    🧪 test(frontend): configura o vitest com RTL

---
+++ app/frontend/vitest.config.ts
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+/// <reference types="next/client" />
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'c8'
+    }
+  }
+});
